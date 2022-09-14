51c27c7e0365a098ff5929570898fa4fc4a9dc26
Arthas Menethil <50101331+XeNune@users.noreply.github.com>
2022-09-14 21:01:25 +0300

    Add files via upload

---
+++ Lecture1/pr4.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <set>
+#include <map>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+int main(int argc, char* argv[])
+{
+	ofstream of;
+	of.open("myfile.txt");
+	for (int i = 0; i < 10; i++) {
+		of << i << endl;
+	}
+	string s;
+	s = "test";
+	cout << s << endl;
+	getchar();
+	return 0;
+}
