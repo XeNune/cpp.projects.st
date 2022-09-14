5bb73de041b05bdb3784c45c53a3504805266de6
Arthas Menethil <50101331+XeNune@users.noreply.github.com>
2022-09-14 20:59:36 +0300

    Add files via upload

---
+++ Lecture1/pr3.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <set>
+#include <map>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+int main(int argc, char* argv[])
+{
+	string s;
+	s = "test";
+	cout << s << endl;
+	getchar();
+	return 0;
+}
