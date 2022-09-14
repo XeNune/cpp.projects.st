578a94ae04fce7f2e381cf449de6d4e5cbde237d
Arthas Menethil <50101331+XeNune@users.noreply.github.com>
2022-09-14 20:59:06 +0300

    Add files via upload

---
+++ Lecture1/pr2.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream> // stdafx.h - это стандартное название предварительно скомпилированного заголовочного файла в Visual Studio 2017 и более ранних версиях. А это VS 2022
+#include <fstream>
+#include <string>
+#include <set>
+#include <map>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	return 0;
+}
