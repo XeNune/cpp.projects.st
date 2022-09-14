08259edf92b7a241c7078cdd89d70b10368a0912
Arthas Menethil <50101331+XeNune@users.noreply.github.com>
2022-09-14 21:01:44 +0300

    Add files via upload

---
+++ Lecture1/pr5.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <set>
+#include <map>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+int main(int argc, char* argv[])
+{
+	ifstream ifile;
+	ifile.open("myfile.txt");
+	string s0, s1;
+	for (;;) {
+		ifile >> s0;
+		if (ifile.eof()) break;
+		s1 += s0 + "\n";
+	} 
+	std::cout << s1;
+	return 0;
+}
