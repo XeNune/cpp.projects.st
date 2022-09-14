a3313a988a36a10315d07e3d7a1244166cadb1d7
Arthas Menethil <50101331+XeNune@users.noreply.github.com>
2022-09-14 21:01:59 +0300

    Add files via upload

---
+++ Lecture1/pr6.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <set>
+#include <map>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	ifstream ifile;
+	ifile.open("myfile.txt");
+	int a;
+	vector<int> v;
+	for (;;) {
+		ifile >> a;
+		if (ifile.eof()) break;
+		v.push_back(a);
+	}
+	for (int i = 0; i < v.size(); i++) {
+		cout << v[i] << endl;
+	}
+	return 0;
+}
